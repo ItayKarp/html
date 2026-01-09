d784b6580efee39b5a5704294a9ffe66a9605de3
ItayKarp <itaykarp@gmail.com>
2026-01-09 10:16:08 +0200

    another one

---
+++ why.js
@@ -0,0 +1,12 @@
+let day = "Sunday"
+switch(day){
+    case "Sunday":
+        console.log("Today is Sunday");
+    case "Monday":
+        console.log("Today is Monday");
+    case "Tuesday":
+        console.log("Today is Tuesday");
+        
+        
+        
+}
