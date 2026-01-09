03eeeab74914bca257d1d4de9361d71ce3b91475
Badonke <itaykarp@gmail.com>
2026-01-09 10:26:38 +0200

    newer_model

--- js_try.js
+++ js_try.js
@@ -1,14 +1,20 @@
-let color = "Yellow"
-switch(color){
-    case "Green":
-        console.log("Go!");
+let month = "a"
+switch(month){
+    case "3":case "2":case "1":
+        console.log("Winter");
         break
-    case "Red":
-        console.log("Stop!!");
+    case "3":case "4":case "5":
+        console.log("Spring");
         break
-    case "Yellow":
-        console.log("Slow Down!");
+    case "6":case "7":case "8":
+        console.log("Summer");
+        break
+    case "9":case "11":case "10":
+        console.log("Fall");
         break
     default:
-        console.log("Null");
+        console.log("Invalid Month");
+        
+        
+        
 }
