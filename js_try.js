bc8d7e9877d748ca255ed01707a2ed78b5af29d7
Badonke <itaykarp@gmail.com>
2026-01-09 10:14:06 +0200

    new_model

---
+++ js_try.js
@@ -0,0 +1,14 @@
+let color = "Yellow"
+switch(color){
+    case "Green":
+        console.log("Go!");
+        break
+    case "Red":
+        console.log("Stop!!");
+        break
+    case "Yellow":
+        console.log("Slow Down!");
+        break
+    default:
+        console.log("Null");
+}
